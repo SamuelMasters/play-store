b750253b3a093eb8686596f9fa6c02971a8bf09e
SamuelMasters <samuelmastersdev@gmail.com>
2023-02-18 13:36:51 +0000

    Setup views, url and template for user profiles

--- play_store/urls.py
+++ play_store/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('user_profiles/', include('user_profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

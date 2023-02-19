6ed3bdfad45350f47d15b242f38035de7d907d41
SamuelMasters <samuelmastersdev@gmail.com>
2023-02-19 13:35:51 +0000

    Add UserProfile form to render on user_profile view

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -7,16 +7,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class UserProfile(models.Model):
-    """ """
+    """ A model to represent a user's saved information """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = PhoneNumberField(null=True, blank=True)
-    default_full_name = models.CharField(max_length=60, null=True, blank=True)
-    default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=90, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=90, null=True, blank=True)
     default_postcode = models.CharField(max_length=25, null=True, blank=True)
     default_county = models.CharField(max_length=90, null=True, blank=True)
-    default_country = CountryField(null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -24,7 +22,7 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_update_profile(sender, instance, created, **kwargs):
-    """ """
+    """ Handles the creation or updating of a user profile """
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def user_profile(request):
-    """ """
+    """
+    A view for displaying a user's saved address information and order history
+    """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)

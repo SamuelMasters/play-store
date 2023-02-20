13885ca53f0a2faf460995ceba717b0f344d6fab
SamuelMasters <samuelmastersdev@gmail.com>
2023-02-20 21:37:09 +0000

    add checkout data cache & customer details to payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,11 @@
+/*
+    Payment flow adapted from:
+    https://stripe.com/docs/payments/accept-a-payment
+    
+    CSS copied from:
+    https://stripe.com/docs/stripe-js
+*/
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -51,9 +59,35 @@ form.addEventListener('submit', function (ev) {
         'disabled': true
     });
     $('#submit-button').attr('disabled', true);
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                email: $.trim(form.email.value),
+                phone_number: $.trim(form.phone_number.value),
+                address:{
+                    street_address1: $.trim(form.street_address1.value),
+                    street_address2: $.trim(form.street_address2.value),
+                    county: $.trim(form.county.value),
+                    country: $.trim(form.country.value),
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone_number: $.trim(form.phone_number.value),
+                address:{
+                    street_address1: $.trim(form.street_address1.value),
+                    street_address2: $.trim(form.street_address2.value),
+                    county: $.trim(form.county.value),
+                    postcode: $.trim(form.postcode.value),
+                    country: $.trim(form.country.value),
+                }
+            }
         }
     }).then(function (result) {
         if (result.error) {
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -10,6 +11,7 @@ from user_profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
+import json
 
 
 # Below views adapated for use here from Boutique Ado project
@@ -20,7 +22,6 @@ def checkout(request):
     """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
@@ -129,3 +130,21 @@ def checkout_success(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@require_POST
+def cache_checkout_data(request):
+    """ Adds checkout metadata to payment intent """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'username': request.user,
+            'save_info': request.POST.get('save_info'),
+            'bag': json.dumps(request.session.get('bag', {})),
+        })
+        return HttpResponse(status=200)
+    except Exception as exception:
+        messages.error(request, 'Unfortunately your pamyent cannot be \
+            processed at the moment. Please try again later.')
+        return HttpResponse(content=exception, status=400)

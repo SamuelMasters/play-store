945132c99bdb68cbaeacb410c5fe8f6bc8225a53
SamuelMasters <samuelmastersdev@gmail.com>
2023-03-01 11:55:32 +0000

    Add privacy policy

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def privacy(request):
+    """ A view to return the privacy policy """
+
+    return render(request, 'home/privacy_policy.html')

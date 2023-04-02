27bcdf51228ceb47993e9cabc19f97bf909cc523
SamuelMasters <samuelmastersdev@gmail.com>
2023-04-02 20:40:25 +0000

    Add sendgrid email configuration to settings.py

--- play_store/settings.py
+++ play_store/settings.py
@@ -117,13 +117,13 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = "test@test.com"
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_KEY")
     EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    DEFAULT_FROM_EMAIL = 'smplaystoreapp@gmail.com'
+    EMAIL_HOST_USER = 'apikey'
+    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True

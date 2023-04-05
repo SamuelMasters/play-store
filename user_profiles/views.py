715753b0d182de987f5123f7c3a3f365150b19b7
SamuelMasters <samuelmastersdev@gmail.com>
2023-04-05 14:34:54 +0000

    Add protection for restricted user_profile view from anonymous users

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 
 from .models import UserProfile
@@ -10,25 +10,29 @@ def user_profile(request):
     """
     A view for displaying a user's saved address information and order history
     """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    if request.user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Changes to your profile have \
-                                        been saved.')
+        if request.method == 'POST':
+            form = UserProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Changes to your profile have \
+                                            been saved.')
 
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+        form = UserProfileForm(instance=profile)
+        orders = profile.orders.all()
 
-    template = 'profiles/profile.html'
-    context = {
-        'form': form,
-        'orders': orders,
-    }
+        template = 'profiles/profile.html'
+        context = {
+            'form': form,
+            'orders': orders,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
+    else:
+        messages.error(request, "You must be logged in to view your profile.")
+        return redirect(reverse('home'))
 
 
 def order_history(request, order_number):
